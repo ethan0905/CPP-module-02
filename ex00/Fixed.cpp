366020990c306997e25e0eff94d924595d0d8443
Ethan SAFAR <esafar@e1r5p10.clusters.42paris.fr>
2022-07-13 18:26:41 +0200

    fixed test_file for ++ operator

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:19:33 by esafar            #+#    #+#             */
-/*   Updated: 2022/07/13 17:43:19 by esafar           ###   ########.fr       */
+/*   Updated: 2022/07/13 18:04:57 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,22 @@
 
 Fixed::Fixed( void ) : _n(0) {
     
+    std::cout << "Default constructor called" << std::endl;
+
     return ;
 }
 
 Fixed::~Fixed() {
+
+    std::cout << "Destructor called" << std::endl;
 
     return ;
 }
 
 int Fixed::getRawBits( void ) const {
 
+    std::cout << "getRawBits member function called" << std::endl;
+    
     return (this->_n);
 }
 
--- ex00/Increm.cpp
+++ ex00/Increm.cpp
@@ -6,33 +6,60 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:47:14 by esafar            #+#    #+#             */
-/*   Updated: 2022/07/13 17:49:43 by esafar           ###   ########.fr       */
+/*   Updated: 2022/07/13 18:14:15 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-using namespace std;
+// using namespace std;
   
-class Count {
+class Fixed {
 
-   public:  
-       Count(int n){
-        _i = n;
-       }  
-       void operator ++() {   
-          _i = _i+1;   
-       }  
-       void afficher() {   
-           cout << "Compteur = " << _i << endl;   
-       }  
+   public:
+
+        Fixed(void);
+        Fixed(int n);
+        ~Fixed(void);
+        
+        // Fixed( int n ){
+        
+        //     _i = n;
+        // }
+       
+       void operator++( void ); 
+       void afficher( void );
 
    private:  
       int _i;  
 };
-  
+
+Fixed::Fixed( int n ){
+
+    _i = n;
+    return ;
+}
+
+Fixed::~Fixed( void ){
+
+    return ;
+}
+
+void    Fixed::operator++( void ) {
+    
+    _i = _i + 1;
+    return ;  
+}
+
+void Fixed::afficher( void ) {
+    
+    std::cout << "Compteur = " << _i << std::endl;   
+    return ;
+}
+
 int main()  
 {  
-    Count c(42);  
+    Fixed c(42);  
+
     ++c;  // appel de la fonction "void operator ++()" 
     c.afficher();  
     return 0;  
