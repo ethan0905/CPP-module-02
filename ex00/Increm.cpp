a740662af4d5967fc8fed316ddc161c72da2555f
Ethan SAFAR <esafar@e1r5p10.clusters.42paris.fr>
2022-07-13 18:36:22 +0200

    implemented operator overload =

--- ex00/Increm.cpp
+++ ex00/Increm.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:47:14 by esafar            #+#    #+#             */
-/*   Updated: 2022/07/13 18:14:15 by esafar           ###   ########.fr       */
+/*   Updated: 2022/07/13 18:35:37 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,63 @@ class Fixed {
       int _i;  
 };
 
+
+class Personne {
+
+   public:
+   
+   // Constructeur paramétré
+    Personne(std::string n, int a);
+   ~Personne(void);
+   
+   // Surcharge de l'opérateur d'affectation
+    void operator=(const Personne &p);
+   void afficher();
+   
+   private:
+      std::string nom;            
+      int age;           
+};
+
+Personne::Personne(std::string n, int a) {
+        nom = n;
+        age = a;
+}
+
+Personne::~Personne(void){
+
+    return ;
+}
+
+void Personne::operator=(const Personne &p) { 
+        nom = p.nom;
+        age = p.age;
+}
+    
+void Personne::afficher() {
+      std::cout << "Le nom de la personne est :" << nom << std::endl;
+      std::cout << "L'age de la personne est :" << age << std::endl;
+}
+
+int main()
+{
+  // Crée deux objets Personne
+   Personne p1("Alex", 25);
+   Personne p2("Bob", 30); 
+   
+   std::cout << "****** Personne 1 ******" << std::endl; 
+   p1.afficher();
+   std::cout << "****** Personne 2 ******" << std::endl; 
+   p2.afficher();
+   // utiliser l'opérateur d'affectation
+   p1 = p2;
+   
+   std::cout << "****** Aprés l'affectation ******" << std::endl; 
+   p1.afficher();
+   return 0;
+}
+
+
 Fixed::Fixed( int n ){
 
     _i = n;
@@ -56,11 +113,11 @@ void Fixed::afficher( void ) {
     return ;
 }
 
-int main()  
-{  
-    Fixed c(42);  
+// int main()  
+// {  
+//     Fixed c(42);  
 
-    ++c;  // appel de la fonction "void operator ++()" 
-    c.afficher();  
-    return 0;  
-}  
+//     ++c;  // appel de la fonction "void operator ++()" 
+//     c.afficher();  
+//     return 0;  
+// }  
